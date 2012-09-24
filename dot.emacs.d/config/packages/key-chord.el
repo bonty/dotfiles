eca04cf83ef8ddb1048ad1438c9f7434f397f5a2
bonty <bonty.shushusha@gmail.com>
2012-09-24 15:12:25 +0900

    add key-chord.el

---
+++ dot.emacs.d/config/packages/key-chord.el
@@ -0,0 +1,8 @@
+;; -*- mode: emacs-lisp ; coding: utf-8 -*-
+
+(el-get 'sync '(key-chord))
+
+(require 'key-chord)
+(setq key-chord-two-keys-delay 0.04)
+(key-chord-mode t)
+(key-chord-define-global "jk" 'view-mode)
