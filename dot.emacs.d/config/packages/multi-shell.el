755e52e7fefc206532de62444ded72b8960335ed
bonty <bonty.shushusha@gmail.com>
2012-10-31 15:20:57 +0900

    add multi-shell

---
+++ dot.emacs.d/config/packages/multi-shell.el
@@ -0,0 +1,8 @@
+;; -*- mode: emacs-lisp ; coding: utf-8 -*-
+
+(el-get 'sync '(multi-shell))
+
+(require 'multi-shell)
+
+(setq multi-shell-revert-window-after-complete nil)
+(setq multi-shell-revert-window-keystroke (kbd "q"))
