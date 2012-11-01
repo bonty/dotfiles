b234c34d50d5ef2a752f0a617cf975986594f24f
bonty <bonty.shushusha@gmail.com>
2012-11-01 18:25:59 +0900

    add xargs alias

---
+++ zsh.d/config/zsh-completions.zsh
@@ -0,0 +1,5 @@
+# zsh-completions config
+
+package-install github zsh-users/zsh-completions
+
+fpath=($(package-directory zsh-users/zsh-complations)/src(N-/) $fpath)
--- zshrc
+++ zshrc
@@ -5,3 +5,4 @@ source ~/.zsh.d/zshrc
 # packages
 source ~/.zsh.d/package.zsh
 source ~/.zsh.d/config/auto-fu.zsh
+source ~/.zsh.d/config/zsh-completions.zsh
