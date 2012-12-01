be8770aeaa0ecbeb339b17156432b75326fdda7e
bonty <bonty.shushusha@gmail.com>
2012-12-01 23:33:27 +0900

    scss-modeの設定

--- emacs.d/config/packages/scss-mode.el
+++ emacs.d/config/packages/scss-mode.el
@@ -2,4 +2,13 @@
 
 (el-get 'sync '(scss-mode))
 
-(require 'scss-mode)
+(autoload 'scss-mode "scss-mode")
+(setq scss-compile-at-save nil)
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+
+(add-hook 'scss-mode-hook
+          (lambda ()
+            (setq css-indent-offset 2)
+            (setq scss-compile-at-save nil)
+            (flymake-mode t)
+            ))
