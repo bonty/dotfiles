6698a01dd449893ee82d8668d99656c19314ef37
bonty <bonty.shushusha@gmail.com>
2013-02-20 17:19:25 +0900

    add rspec-mode and feature-mode(cucumber)

---
+++ emacs.d/config/packages/feature-mode.el
@@ -0,0 +1,6 @@
+;; -*- mode: emacs-lisp ; coding: utf-8 -*-
+
+(el-get 'sync '(feature-mode))
+
+(add-to-list 'load-path "~/.emacs.d/packages/feature-mode")
+(require 'feature-mode)
---
+++ emacs.d/config/packages/rspec-mode.el
@@ -0,0 +1,6 @@
+;; -*- mode: emacs-lisp ; coding: utf-8 -*-
+
+(el-get 'sync '(rspec-mode))
+
+(add-to-list 'load-path "~/.emacs.d/packages/rspec-mode")
+(require 'rspec-mode)
