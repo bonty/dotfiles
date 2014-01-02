6bcc2ca57e357e0c1990b4c7a34bfbd5a3a4165d
bonty <bonty.shushusha@gmail.com>
2014-01-02 21:56:20 +0900

    remove color-theme.el

--- home/.emacs.d/config/packages/color-theme.el
+++
@@ -1,6 +0,0 @@
-;; -*- mode: emacs-lisp ; coding: utf-8 -*-
-
-(el-get 'sync '(color-theme-almost-monokai))
-(setq color-theme-libraries (directory-files "~/.emacs.d/packages/color-theme/themes" t "^color-theme"))
-
-(color-theme-almost-monokai)
