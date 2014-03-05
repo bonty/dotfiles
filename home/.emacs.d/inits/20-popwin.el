b0acf4f23c5c61224626b7b63a472b915bdb490b
bonty <bonty.shushusha@gmail.com>
2014-03-05 13:22:43 +0900

    fix typo
    avoid emacs 24.3 timer-list bug

--- home/.emacs.d/inits/20-popwin.el
+++ home/.emacs.d/inits/20-popwin.el
@@ -1,6 +1,6 @@
 (setq pop-up-windows nil)
 (require 'popwin)
-(setq popwin:close-popup-window-timre-interval 0.5)
+(setq popwin:close-popup-window-timer-interval 0.5)
 
 (setq display-buffer-function 'popwin:display-buffer)
 (push '("*Completions*" :height 0.4) popwin:special-display-config)
