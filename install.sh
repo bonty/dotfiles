44079296115ac4c5934c7f18b51181bdc58a9636
bonty <bonty.shushusha@gmail.com>
2014-05-02 02:34:00 +0900

    add install.sh and touch setup shellscript

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+export HOMEBREW_CASK_OPTS="--appdir=/Applications --caskroom=/usr/local/Caskroom"
+
+CURRENT_DIR=`dirname $0`
+
+cd $CURRENT_DIR
+
+# install homebrew if not installed
+if [ ! `which brew` ]; then
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+fi
+
+# install software with homebrew
+brew bundle
+
+# setup with shell scripts
+find $CURRENT_DIR/setups -name "*.sh" | xargs -I S source S
