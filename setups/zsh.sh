c93dd1d2bde70efe9f3d578842cf5808a8e49630
bonty <bonty.shushusha@gmail.com>
2014-05-07 16:50:15 +0900

    fix chsh exec condition

--- setups/zsh.sh
+++ setups/zsh.sh
@@ -2,7 +2,9 @@
 
 # chsh
 path_zsh=$(find $(brew --prefix)/bin -name zsh)
-if [ -n ${path_zsh} -a $SHELL != ${path_sh} ]; then
-    echo ${path_zsh} | sudo tee -a /etc/shells
-    chsh -s ${path_zsh}
+if [ -n ${path_zsh} ]; then
+    if [ $SHELL != ${path_zsh} ]; then
+        echo ${path_zsh} | sudo tee -a /etc/shells
+        chsh -s ${path_zsh}
+    fi
 fi
