a9adf1a6912ca0cba96b57ca4760b277eb9c6bea
bonty <bonty.shushusha@gmail.com>
2014-05-07 16:47:47 +0900

    fix install comannd for percol

--- setups/python.sh
+++ setups/python.sh
@@ -6,7 +6,7 @@ pyenv global 2.7.6
 pyenv rehash
 
 # install percol
-pyenv exec pip install percol
+pyenv exec pip install percol --allow-external percol --allow-unverified percol
 
 # install awscli
 pyenv exec pip install awscli
