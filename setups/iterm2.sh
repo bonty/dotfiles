f1f29d1fe64b5a49dd197af043d3e4a8377eeae8
bonty <bonty.shushusha@gmail.com>
2014-05-07 17:24:04 +0900

    fix syntax

--- setups/iterm2.sh
+++ setups/iterm2.sh
@@ -5,7 +5,7 @@ THEMEDIR=`dirname $0`/../themes/iterm2/
 cd $THEMEDIR
 
 for f in *; do
-    THEME = $(basename "$f")
+    THEME=$(basename "$f")
     defaults write -app iTerm 'Custom Color Presets' -dict-add "$THEME" "$(cat " $f")"
 done
 
