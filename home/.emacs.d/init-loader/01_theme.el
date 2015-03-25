33b0d45fc10e48cc7faad14784177a57476aac36
bonty <bonty.shushusha@gmail.com>
2015-03-25 14:57:11 +0900

    remove

--- home/.emacs.d/init-loader/01_theme.el
+++
@@ -1,4 +0,0 @@
-(el-get-lock-unlock 'reverse-theme)
-(el-get-bundle! reverse-theme
-  (with-eval-after-load-feature 'reverse-theme
-    (load-theme 'reverse t)))
--- home/.emacs.d/init-loader/02_highlight.el
+++
@@ -1,4 +0,0 @@
-;; color basic config files
-(require 'generic-x)
-
-(global-font-lock-mode t)
