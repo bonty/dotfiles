0bfe981bd17e833d62970a1dee2ac038bcc254b4
bonty <bonty.shushusha@gmail.com>
2015-03-25 19:19:18 +0900

    setup config

--- home/.emacs.d/init-loader/01_basic.el
+++ home/.emacs.d/init-loader/01_basic.el
@@ -62,3 +62,62 @@
 (require 'server)
 (unless (server-running-p)
   (server-start))
+
+
+;; migemo
+(bundle! emacs-jp/migemo
+  (with-eval-after-load-feature 'migemo
+    (setq migemo-command "cmigemo")
+    (setq migemo-options '("-q" "--emacs"))
+
+    ;; install cmigemo by homebrew
+    (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+
+    (setq migemo-user-dictionary nil)
+    (setq migemo-regex-dictionary nil)
+    (setq migemo-coding-system 'utf-8)
+    (load-library "migemo")
+    (migemo-init)))
+
+;; ;; helm
+(custom-set-variables
+ '(helm-input-idle-delay 0)
+ '(helm-exit-idle-delay 0)
+ '(helm-candidate-number-limit 500)
+ '(helm-boring-file-regexp-list '("~$"))
+ '(helm-mini-default-sources '(helm-source-buffers-list
+                               helm-source-recentf
+                               ;; helm-source-ls-git
+                               helm-source-files-in-current-dir
+                               helm-source-mac-spotlight
+                               helm-source-buffer-not-found)))
+
+(bundle! helm
+  (with-eval-after-load-feature 'helm-config
+    (helm-mode t))
+
+  (with-eval-after-load-feature 'helm
+    (global-set-key (kbd "C-x b") 'helm-mini)
+    (global-set-key (kbd "M-x") 'helm-M-x)
+    (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+    (define-key helm-map (kbd "C-h") 'delete-backward-char))
+
+  (with-eval-after-load-feature 'helm-files
+    (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+    (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)))
+
+;; (bundle! helm-descbinds
+;;   (with-eval-after-load-feature 'helm-descbinds
+;;     (helm-descbinds-mode)))
+
+;; ;; (el-get-bundle! helm-ls-git)
+
+;; (bundle! helm-migemo
+;;   (with-eval-after-load-feature 'helm-migemo
+;;     (setq helm-use-migemo t)))
+
+(bundle! helm-swoop
+  (with-eval-after-load-feature 'helm-swoop
+    (define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
+    (define-key helm-swoop-map (kbd "C-s") 'helm-next-line)))
+;; ;; (el-get-bundle! ace-isearch)
