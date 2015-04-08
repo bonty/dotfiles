d173605dfa5fc61c75cb27cdc74daffc7347bcd8
bonty <bonty.shushusha@gmail.com>
2015-04-08 15:16:06 +0900

    update karabiner settings

--- setups/karabiner.sh
+++ setups/karabiner.sh
@@ -2,8 +2,6 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set remap.app_term_commandL2optionL_except_tab 1
-/bin/echo -n .
 $cli set repeat.initial_wait 300
 /bin/echo -n .
 $cli set remap.space2shiftL_space_fnspace 1
@@ -20,9 +18,8 @@ $cli set private.capslock_hack_f19 1
 /bin/echo -n .
 $cli set remap.controlL2controlL_escape 1
 /bin/echo -n .
-$cli set remap.shiftL2commandL 1
+$cli set remap.shiftL2optionL 1
 /bin/echo -n .
 $cli set private.control_to_option_on_apple_keyboard 1
 /bin/echo -n .
 /bin/echo
-
