c3905a0b720a19401c2c454915e970cfbcc5d9ef
bonty <bonty.shushusha@gmail.com>
2015-09-14 00:45:34 +0900

    Revert "do not use homesick. to sync dotfiles using mackup"
    This reverts commit 62d77298799655b9ffcb77decc6c087f0def5ac4.

--- setups/ruby.sh
+++ setups/ruby.sh
@@ -3,3 +3,11 @@
 rbenv install 2.2.3
 rbenv global 2.2.3
 rbenv rehash
+
+rbenv exec gem install homesick
+rbenv rehash
+
+# setup homesick
+rbenv exec homesick clone bonty/dotfiles
+rbenv exec homesick pull dotfiles
+yes | rbenv exec homesick symlink dotfiles
