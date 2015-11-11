1d6497def5cdbbdf63e32e84e0fa168686d641de
bonty <bonty.shushusha@gmail.com>
2015-11-11 13:59:31 +0900

    change junk file path

--- home/.emacs.d/init-loader/30_open-junk-file.el
+++ home/.emacs.d/init-loader/30_open-junk-file.el
@@ -1,2 +1,2 @@
-(setq open-junk-file-format "~/Dropbox/junk/%Y-%m/%Y-%m-%d-%H%M%S.")
+(setq open-junk-file-format "~/Dropbox/junk/%Y/%Y-%m-%d-%H%M%S.")
 (global-set-key (kbd "C-x j") 'open-junk-file)
