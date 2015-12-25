2cfabccadc396de456381fbd1c48d7273a1d80df
bonty <bonty.shushusha@gmail.com>
2015-12-25 11:57:48 +0900

    update init.el

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,13 +1,21 @@
+(unless load-file-name
+  (cd (getenv "HOME")))
+
+(require 'cl-lib)
+
 ;; enable to test temporary .emacs
 ;; usage: emacs -q -l ~/path/to/init.el
 (when load-file-name
-  (setq user-emacs-directory (file-name-directory load-file-name)))
+  (setq-default user-emacs-directory (file-name-directory load-file-name)))
 
 ;; el-get bundle
 (load (concat user-emacs-directory "el-get-bundle.el"))
 
 ;; load environment variables
-(exec-path-from-shell-initialize)
+;; don't check .zshrc
+(custom-set-variables
+ '(exec-path-from-shell-check-startup-files nil))
+(exec-path-from-shell-copy-envs '("PATH"))
 
 ;; loading theme
 (load-theme 'reverse t t)
@@ -16,10 +24,4 @@
 ;; init-loader
 (custom-set-variables
  '(init-loader-show-log-after-init 'error-only))
-;; '(init-loader-byte-compile t))
-
 (init-loader-load (locate-user-emacs-file "init-loader"))
-
-;; hide compilation results
-;; (let ((win (get-buffer-window "*Compile-Log*")))
-;;   (when win (delete-window win)))
