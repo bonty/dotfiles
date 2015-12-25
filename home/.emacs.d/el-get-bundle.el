e3d18ef949e8dd577adc112a32a467c55f8751f7
bonty <bonty.shushusha@gmail.com>
2015-12-25 17:43:25 +0900

    comment out all packages

--- home/.emacs.d/el-get-bundle.el
+++ home/.emacs.d/el-get-bundle.el
@@ -22,95 +22,95 @@
 (el-get-bundle syohex/emacs-reverse-theme :name reverse-theme
   (add-to-list 'custom-theme-load-path default-directory))
 
-;; undo
-(el-get-bundle undo-tree)
-(el-get-bundle m2ym/undohist-el :name undohist)
+;; ;; undo
+;; (el-get-bundle undo-tree)
+;; (el-get-bundle m2ym/undohist-el :name undohist)
 
-;; newline
-(el-get-bundle smart-newline)
+;; ;; newline
+;; (el-get-bundle smart-newline)
 
-;; search
-(el-get-bundle syohex/emacs-anzu :name anzu)
+;; ;; search
+;; (el-get-bundle syohex/emacs-anzu :name anzu)
 
-;; cursor
-;; (el-get-bundle winterTTr/ace-jump-mode)
+;; ;; cursor
+;; ;; (el-get-bundle winterTTr/ace-jump-mode)
 
-;; paren
-(el-get-bundle rainbow-delimiters)
-(el-get-bundle capitaomorte/autopair)
+;; ;; paren
+;; (el-get-bundle rainbow-delimiters)
+;; (el-get-bundle capitaomorte/autopair)
 
-;; region
-(el-get-bundle expand-region)
+;; ;; region
+;; (el-get-bundle expand-region)
 
-;; buffer utils
-(el-get-bundle m2ym/popwin-el :name popwin)
-;; (el-get-bundle lukhas/buffer-move)
-(el-get-bundle kentaro/auto-save-buffers-enhanced)
-(el-get-bundle recentf-ext)
+;; ;; buffer utils
+;; (el-get-bundle m2ym/popwin-el :name popwin)
+;; ;; (el-get-bundle lukhas/buffer-move)
+;; (el-get-bundle kentaro/auto-save-buffers-enhanced)
+;; (el-get-bundle recentf-ext)
 
-;; dired
-;; (el-get-bundle m2ym/direx-el :name direx)
-;; (el-get-bundle syohex/emacs-dired-k :name dired-k)
-;; (el-get-bundle dired-hacks)
+;; ;; dired
+;; ;; (el-get-bundle m2ym/direx-el :name direx)
+;; ;; (el-get-bundle syohex/emacs-dired-k :name dired-k)
+;; ;; (el-get-bundle dired-hacks)
 
-;; auto-complete
-(el-get-bundle auto-complete/popup-el :name popup)
-(el-get-bundle auto-complete/fuzzy-el :name fuzzy)
-(el-get-bundle auto-complete/auto-complete)
+;; ;; auto-complete
+;; (el-get-bundle auto-complete/popup-el :name popup)
+;; (el-get-bundle auto-complete/fuzzy-el :name fuzzy)
+;; (el-get-bundle auto-complete/auto-complete)
 
-;; helm
-(el-get-bundle helm)
-(el-get-bundle helm-descbinds)
-(el-get-bundle helm-swoop)
-(el-get-bundle helm-ls-git)
-(el-get-bundle helm-git-grep)
-(el-get-bundle helm-ag)
+;; ;; helm
+;; (el-get-bundle helm)
+;; (el-get-bundle helm-descbinds)
+;; (el-get-bundle helm-swoop)
+;; (el-get-bundle helm-ls-git)
+;; (el-get-bundle helm-git-grep)
+;; (el-get-bundle helm-ag)
 
-;; flycheck
-(el-get-bundle flycheck)
+;; ;; flycheck
+;; (el-get-bundle flycheck)
 
-;; git
-(el-get-bundle magit)
-(el-get-bundle syohex/emacs-git-gutter :name git-gutter)
+;; ;; git
+;; (el-get-bundle magit)
+;; (el-get-bundle syohex/emacs-git-gutter :name git-gutter)
 
-;; major mode
-;; ruby
-(el-get-bundle ruby-block)
-(el-get-bundle ruby-end)
-(el-get-bundle inf-ruby)
+;; ;; major mode
+;; ;; ruby
+;; (el-get-bundle ruby-block)
+;; (el-get-bundle ruby-end)
+;; (el-get-bundle inf-ruby)
 
-;; yaml
-(el-get-bundle yaml-mode)
+;; ;; yaml
+;; (el-get-bundle yaml-mode)
 
-;; javascript
-(el-get-bundle js2-mode)
-(el-get-bundle coffee-mode)
+;; ;; javascript
+;; (el-get-bundle js2-mode)
+;; (el-get-bundle coffee-mode)
 
-;; markdown
-(el-get-bundle markdown-mode)
+;; ;; markdown
+;; (el-get-bundle markdown-mode)
 
-;; dockerfile
-(el-get-bundle dockerfile-mode)
+;; ;; dockerfile
+;; (el-get-bundle dockerfile-mode)
 
-;; groovy(for gradle)
-(el-get-bundle groovy-emacs-mode)
+;; ;; groovy(for gradle)
+;; (el-get-bundle groovy-emacs-mode)
 
-;; lua
-(el-get-bundle lua-mode)
+;; ;; lua
+;; (el-get-bundle lua-mode)
 
-;; csharp
-(el-get-bundle josteink/csharp-mode)
-(el-get-bundle OmniSharp/omnisharp-emacs)
+;; ;; csharp
+;; (el-get-bundle josteink/csharp-mode)
+;; (el-get-bundle OmniSharp/omnisharp-emacs)
 
-;; shell
-(el-get-bundle syohex/emacs-quickrun :name quickrun)
+;; ;; shell
+;; (el-get-bundle syohex/emacs-quickrun :name quickrun)
 
-;; document
-(el-get-bundle dash-at-point)
+;; ;; document
+;; (el-get-bundle dash-at-point)
 
-;; auto-complete plugins
-(el-get-bundle zk-phi/ac-c-headers)
-(el-get-bundle dgutov/robe)
+;; ;; auto-complete plugins
+;; (el-get-bundle zk-phi/ac-c-headers)
+;; (el-get-bundle dgutov/robe)
 
-;; open-junk-file
-(el-get-bundle open-junk-file)
+;; ;; open-junk-file
+;; (el-get-bundle open-junk-file)
