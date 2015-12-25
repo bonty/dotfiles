37f256d8c756731c67709e7838ab0d645a6de2f7
bonty <bonty.shushusha@gmail.com>
2015-12-25 16:08:44 +0900

    rename search config

--- home/.emacs.d/init-loader/02_search.el
+++ home/.emacs.d/init-loader/02_search.el
@@ -3,4 +3,5 @@
 (custom-set-variables
  '(anzu-mode-lighter "")
  '(anzu-deactivate-region t)
- '(anzu-search-threshold 1000))
+ '(anzu-search-threshold 1000)
+ '(anzu-replace-to-string-separator " => "))
