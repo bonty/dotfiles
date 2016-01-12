4bdf17e9ef70fff5bf7efa8529f6e712597cf73c
bonty <bonty.shushusha@gmail.com>
2016-01-12 17:44:21 +0900

    add mkdir

--- setups/mysql.sh
+++ setups/mysql.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+if [ ! -d ~/Library/LaunchAgents ]; then
+    mkdir ~/Library/LaunchAgents
+fi
+
 ln -sfv $(brew --prefix)/opt/mysql/*.plist ~/Library/LaunchAgents/
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
--- setups/redis.sh
+++ setups/redis.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+if [ ! -d ~/Library/LaunchAgents ]; then
+    mkdir ~/Library/LaunchAgents
+fi
+
 ln -sfv $(brew --prefix)/opt/redis/*.plist ~/Library/LaunchAgents/
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.redis.plist
