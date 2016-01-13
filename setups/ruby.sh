9c1247b8309c18d53a0ed8a43d1c7de5265d7db0
bonty <bonty.shushusha@gmail.com>
2016-01-13 16:34:08 +0900

    add bundler and ec2ssh

--- setups/ruby.sh
+++ setups/ruby.sh
@@ -4,7 +4,9 @@ rbenv install 2.2.3
 rbenv global 2.2.3
 rbenv rehash
 
+rbenv exec gem install bundler
 rbenv exec gem install homesick
+rbenv exec gem install ec2ssh
 rbenv rehash
 
 # setup homesick
