9d7feca274311364cb6d07b4b0a68123a1a9e3eb
bonty <bonty.shushusha@gmail.com>
2016-01-20 14:42:18 +0900

    remove aws tool from pyenv. they are managed by homebrew now.

--- setups/python.sh
+++ setups/python.sh
@@ -5,8 +5,6 @@ pyenv global 2.7.8
 pyenv rehash
 
 pyenv exec pip install percol --allow-external percol --allow-unverified percol
-pyenv exec pip install awscli
 pyenv exec pip install pygments
-pyenv exec pip install boto
 
 pyenv rehash
