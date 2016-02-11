1f8f25cd8fafc837be66531b78d79c0f950ef664
bonty <bonty.shushusha@gmail.com>
2016-02-11 23:30:23 +0900

    load from local

---
+++ home/.zsh.d/10_keybinds.zsh
@@ -0,0 +1,7 @@
+bindkey -e
+
+has 'history-substring-search-up' &&
+    bindkey '^P' history-substring-search-up
+has 'history-substring-search-down' &&
+    bindkey '^N' history-substring-search-down
+
