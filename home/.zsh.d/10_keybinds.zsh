c78a7cad5012167640834669f90de22d06baf761
bonty <bonty.shushusha@gmail.com>
2016-02-15 20:14:18 +0900

    fix history list

--- home/.zsh.d/10_keybinds.zsh
+++ home/.zsh.d/10_keybinds.zsh
@@ -31,7 +31,7 @@ _fzf-select-history() {
     BUFFER=$(
         history \
             | sort -k1,1nr \
-            | perl -ne 'BEGIN { my @lines = (); } s/^\s*\d+\s*//; $in=$_; if (!(grep {$in eq $_} @lines)) { push(@lines, $in); print $in; }' \
+            | perl -ne 'BEGIN { my @lines = (); } s/^\*?\s*\d+\s*//; $in=$_; if (!(grep {$in eq $_} @lines)) { push(@lines, $in); print $in; }' \
             | fzf --query "$LBUFFER"
           )
 
